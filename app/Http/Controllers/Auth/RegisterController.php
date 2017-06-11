bc07b7f8148c37baa441bb797119efc46c803657
pteric <15652915029@163.com>
2017-06-11 23:49:31 +0800

    完成用户注册并发邮件激活

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Naux\Mail\SendCloudTemplate;
 
 class RegisterController extends Controller
 {
@@ -62,10 +64,31 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'avatar' => '../../../../public/images/avatars/default.png',
+            'confirmation_token' => str_random(40),
             'password' => bcrypt($data['password']),
         ]);
+
+        $this->sendVerifyEmailTo($user);
+        return $user;
+    }
+
+    protected function sendVerifyEmailTo($user)
+    {
+        // 模板变量
+        $bind_data = [
+            'url' => route('email.verify', ['token' => $user->confirmation_token]),
+            'name' => $user->name
+            ];
+        $template = new SendCloudTemplate('zhihu_app_register', $bind_data);
+
+        Mail::raw($template, function ($message) use ($user) {
+            $message->from('peng_tuo@foxmail.com', 'Laravel-zhihu');
+
+            $message->to($user->email);
+        });
     }
 }
